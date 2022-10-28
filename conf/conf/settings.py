c3d893239e9a0af83bc88ae7f5766ed3eb59a15e
JoseUrtubey <joseaurtubey@gmail.com>
2022-10-28 09:51:17 -0300

    Changed db engine to mysql

--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = "conf.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_database',
+        'USER': 'root',
+        'PASSWORD': 'your_password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 

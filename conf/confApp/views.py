02c6aded2705f713c14ac725adbc952726c37862
Mateo <mateo.marchisone@gmail.com>
2022-11-23 10:14:45 -0300

    paciente template

--- conf/confApp/views.py
+++ conf/confApp/views.py
@@ -6,11 +6,11 @@ def home_screen_view(request):
 
 
 def login_screen_view(request):
-    return render(request, "login.html")
+    return render(request, "create_paciente.html")
 
 
 def solicitud_screen_view(request):
-    return render(request, "make_solicitud.html")
+    return render(request, "create_solicitud.html")
 
 
 def resultado_screen_view(request):

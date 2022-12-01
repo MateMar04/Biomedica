8036fcb9e786f932b9e93086d3892d9e07464ca6
MateMar04 <mateo.marchisone@gmail.com>
2022-12-01 17:55:31 -0300

    test doesnt work

--- conf/confApp/tests.py
+++ conf/confApp/tests.py
@@ -1,1 +1,17 @@
 # Create your tests here.
+from django.test import TestCase
+
+from .models import *
+
+
+class MedicoTestCase(TestCase):
+    def setUp(self):
+        Medico.objects.create(nombre="Pepe", apellido="Garcia", matricula="ndgfkjnf")
+        Medico.objects.create(nombre="Andres", apellido="Gonzalez", matricula="adssgfkq")
+
+    def test_medico_is_valid(self):
+        pepe = Medico.objects.get(name="Pepe")
+        andres = Medico.objects.get(name="Andres")
+
+        self.assertEqual(pepe.matricula, "ndgfkjnf")
+        self.assertEqual(andres.apellido, "Gonzalez")
